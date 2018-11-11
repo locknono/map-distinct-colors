32489c8ababf37d107524a80f8e2d729184f26ca
locknono <guozy0103@outlook.com>
2018-11-11 13:41:17 +0800

    Initial implementation of createCluster

--- packages/clusters.js
+++ packages/clusters.js
@@ -6,7 +6,19 @@ type cluster = {
 };
 
 function createCluster(points: Array<point>): Array<cluster> {
+  let clusterMap = new Map();
+  for (let i = 0; i < points.length; i++) {
+    let classNumber = points[i].classNumber;
+    if (clusterMap.get(classNumber) === undefined) {
+      clusterMap.set(classNumber, 1);
+    } else {
+      let area = clusterMap.get(classNumber);
+      clusterMap.set(classNumber, area + 1);
+    }
+  }
   let clusters = [];
-
+  for (let cluster in clusterMap.entries) {
+    clusters.push({ classNumber: cluster[0], area: cluster[1] });
+  }
   return clusters;
 }
